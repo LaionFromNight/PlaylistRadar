e537f3ba815d5b4e769e3f17c26ee0e474987548
LaionFromNight <66757837+LaionFromNight@users.noreply.github.com>
2025-06-12 23:00:37 +0200

    Add sidebar navigation with routing

---
+++ src/HowToStart.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HowToStart() {
+  return (
+    <div>
+      <h1>How To Start</h1>
+      <p>Learn how to get started using PlaylistRadar.</p>
+    </div>
+  );
+}
